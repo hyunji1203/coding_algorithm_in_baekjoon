e5c01c60e937644e9fe4783d503e219b82320cfc
hyunji <83613106+hyunji1203@users.noreply.github.com>
2023-12-22 03:26:22 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1. 주차 요금 A,B,C 입력 받음
+// 2. 3줄에 걸쳐 차 3대의 각각 도착 & 떠난 시간 입력 받음
+// 3. 시간별로 몇대가 주차 되었는지 확인후 돈 계산
+
+int a,b,c;
+int times[110];
+int price = 0;
+
+void initTimes(){
+    fill(&times[0], &times[110], 0);
+}
+
+int main(){
+    initTimes();
+    cin >> a >> b >> c;
+
+    int fstart, fend;
+    int sstart, send;
+    int tstart, tend;
+
+    cin >> fstart >> fend;
+    cin >> sstart >> send;
+    cin >> tstart >> tend;
+
+    for(int i= fstart; i<fend; i++){
+        times[i] += 1;
+    }
+    for(int i= sstart; i<send; i++){
+        times[i] += 1;
+    }
+    for(int i= tstart; i<tend; i++){
+        times[i] += 1;
+    }
+
+    for(int i: times){
+        if(i == 1){
+            price = price + a;
+        } else if (i == 2){
+            price = price + (2 * b);
+        } else if (i == 3){
+            price = price + (3 * c);
+        }
+    }
+
+    cout << price << "\n";
+}
