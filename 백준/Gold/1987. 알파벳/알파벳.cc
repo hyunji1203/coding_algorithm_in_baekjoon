b428e73d5499646db1b060d188f9782a6ef43e53
hyunji <83613106+hyunji1203@users.noreply.github.com>
2024-03-04 21:04:10 +0900

    [Gold IV] Title: 알파벳, Time: 372 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,41 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int R, C, max_value;
-char adj[24][24];
-int visited[30];
-int dy[] = {-1, 0 ,1, 0}, dx[] = {0, 1, 0, -1};
+int R, C, ret = INT_MIN;
+string s;
+char adj[21][21];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
 
-void go(int a, int b, int cnt){
-    max_value = max(max_value, cnt);
-
+void go(int a, int b, int num, int cnt){
+    ret = max(ret, cnt);
     for(int i=0; i<4; i++){
         int ny = a + dy[i];
         int nx = b + dx[i];
-
-        if(ny<0 || ny>=R || nx<0 || nx >= C) continue;
-        if(visited[adj[ny][nx] - 'A']) continue;
-        visited[adj[ny][nx] - 'A'] = 1;
-        go(ny, nx, cnt+1);
-        visited[adj[ny][nx] - 'A'] = 0;
-    }   
-    return;
-} 
+        if(ny<0 || ny >=R || nx<0 || nx>=C) continue;
+        int idx = adj[ny][nx] - 'A';
+        if(num & (1 << idx)) continue;
+        go(ny, nx, num | (1<<idx), cnt+1); 
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> R >> C;
-    for(int i = 0; i < R; i++){
-        for(int j = 0; j < C; j++){
-            cin >> adj[i][j];
+    for(int i=0; i<R; i++){
+        cin >> s;
+        for(int j=0; j<C; j++){
+            adj[i][j] = s[j];
         }
     }
+    int start = pow(2, adj[0][0] - 'A'); 
+    go(0, 0, start, 1);
 
-    visited[adj[0][0] - 'A'] = 1;
-    go(0, 0, 1);
-    cout << max_value << "\n";
+    cout << ret << "\n";
 
     return 0;
 }
