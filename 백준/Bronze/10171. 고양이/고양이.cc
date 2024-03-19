aa1bf36c75b9841b8f33cb9dcd2cea309a162360
hyunji <83613106+hyunji1203@users.noreply.github.com>
2024-03-19 23:59:07 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+
+    cout << "\\    /\\" << "\n";
+    cout << " )  ( ')" << "\n";
+    cout << "(  /  )" << "\n";
+    cout << " \\(__)|" << "\n";
+    return 0;
+}
