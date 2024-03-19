210ff53ff26a6224b8d6586882e69af0d0d44389
hyunji <83613106+hyunji1203@users.noreply.github.com>
2024-03-20 00:04:15 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+
+    cout << "|\\_/|" << "\n";
+    cout << "|q p|   /}" << "\n";
+    cout << "( 0 )\"\"\"\\" << "\n";
+    cout << "|\"^\"`    |" << "\n";
+    cout << "||_/=\\\\__|" << "\n";
+    return 0;
+}
