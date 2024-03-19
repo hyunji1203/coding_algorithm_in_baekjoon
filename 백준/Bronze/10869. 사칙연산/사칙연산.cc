6f51effd170a84578ea2e25466778ad53e09744f
hyunji <83613106+hyunji1203@users.noreply.github.com>
2024-03-19 22:31:55 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> a >> b;
+    cout << a+b << "\n" << a-b << "\n" << a*b << "\n" << a/b << "\n" << a%b << "\n";
+    return 0;
+}
