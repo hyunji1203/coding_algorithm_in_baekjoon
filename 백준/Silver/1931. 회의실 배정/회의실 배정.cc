b826bb21340a79632bb04550953632a95d30c44f
hyunji <83613106+hyunji1203@users.noreply.github.com>
2024-03-28 01:35:18 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, from, to, cnt = 1;
-vector<pair<int, int>> meetings;
+int n, s, e, ret = 1;
+vector<pair<int, int>> v;
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -10,22 +10,24 @@ int main(){
 
     cin >> n;
     for(int i=0; i<n; i++){
-        cin >> from >> to;
-        meetings.push_back({to, from});
+        cin >> s >> e;
+        v.push_back({e, s});
     }
 
-    sort(meetings.begin(), meetings.end());
+    sort(v.begin(), v.end());
 
-    int f = meetings[0].second;
-    int t = meetings[0].first;
+    int starttime = v[0].second;
+    int endtime = v[0].first;
 
     for(int i=1; i<n; i++){
-        if(t > meetings[i].second) continue;
-        f = meetings[i].second;
-        t = meetings[i].first;
-        cnt++;
+        if(endtime <= v[i].second){
+            starttime = v[i].second;
+            endtime = v[i].first;
+            ret++;
+        }
     }
 
-    cout << cnt << "\n";
+    cout << ret << "\n";
+
     return 0;
 }
