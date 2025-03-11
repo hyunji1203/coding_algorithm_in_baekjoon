30a96c4fe852a07d0a13d664868f50a87b2217d3
hyunji <83613106+hyunji1203@users.noreply.github.com>
2025-03-12 01:03:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 185.45 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -6,20 +6,24 @@ map<string, int> mp;
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     
-    for(string it: completion){
-        if(mp[it] == 0){
-            mp[it] = 1;
-        }else{
-            mp[it] = mp[it] + 1;
+    for(int i=0; i<completion.size(); i++) {
+        if(mp[completion[i]] == 0) {
+            mp[completion[i]] = 1;
+        } else {
+            mp[completion[i]] = mp[completion[i]] + 1;
         }
     }
     
-    for(string it: participant){
-        if(mp[it] == 0) {
-            answer = it;
+    for(int i=0; i<participant.size(); i++) {
+        if(mp[participant[i]] != 0) {
+            if(mp[participant[i]] > 1) {
+                mp[participant[i]] = mp[participant[i]] - 1;
+            } else if(mp[participant[i]] == 1) {
+                mp.erase(participant[i]);
+            } 
+        } else {
+            answer = participant[i];
             break;
-        }else{
-            mp[it] = mp[it] - 1;    
         }
     }
     
